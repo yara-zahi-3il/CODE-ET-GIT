b6507bba8b4dea90940c91beca3404ff976a12aa
yara zahi <yzahi2004@gmail.com>
2024-11-26 17:37:22 +0100

    exo1 finit

--- exercice_1/exercice1.java
+++ exercice_1/exercice1.java
@@ -1,17 +1,20 @@
 public class FactureManager {
 
+    /**
+     * Calcule le total d'une facture en appliquant des reductions basee
+     * sur le type de produit et le montant total.
+     * @param typeProduit Le type du produit (Alimentaire, Electronique, Luxe)
+     * @param quantite    La quantite de produits achetes
+     * @param prixUnitaire Le prix unitaire du produit
+     * @return Le montant total apres application des reductions
+     */
+
     // Calcul le total d'une facture
     public double calculerFacture(String typeProduit, int quantite, double prixUnitaire) {
         double total = quantite * prixUnitaire;
-
-        // reduction selon la catégorie
-        if (typeProduit.equals("Alimentaire")) {
-            total -= total * 0.05; // Réduction de 5%
-        } else if (typeProduit.equals("Electronique")) {
-            total -= total * 0.1; // Réduction de 10%
-        } else if (typeProduit.equals("Luxe")) {
-            total -= total * 0.15; // Réduction de 15%
-        }
+        
+        // Appliquer la reduction en fonction du type de produit
+        total -= total*obtenirReductionProduit(typeProduit);
 
         // Reduction sur le total
         if (total > 1000) {
@@ -20,4 +23,26 @@ public class FactureManager {
 
         return total;
     }
+
+
+    /**
+     * Obtient le pourcentage de reduction en fonction du type de produit.
+     *
+     * @param typeProduit Le type du produit
+     * @return Le pourcentage de restatuduction (par exemple 0.05 pour 5%)
+     */
+
+    public double obtenirReductionProduit(String typeProduit){
+        switch(typeProduit){
+            case "Alimentaire" :
+                return 0.05;  // Réduction de 5%
+            case "Electronique" :
+                return 0.10;  // Réduction de 10%
+            case "Luxe" :
+                return 0.15;  // Réduction de 15%
+        }
+    }
+
+
 }
+
