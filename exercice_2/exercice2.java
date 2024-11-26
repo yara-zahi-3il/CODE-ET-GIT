cc533f59612388893beb36c6dbde27311945768f
yara zahi <yzahi2004@gmail.com>
2024-11-26 18:16:00 +0100

    exo2 finit aussi

--- exercice_2/exercice2.java
+++ exercice_2/exercice2.java
@@ -1,21 +1,47 @@
 public class GestionnaireNotes {
 
+
+/** Afficher les notes et la moyenne en meme temps
+ * 
+ * @param nomEtudiant qui est le nom de l'etudiant
+ * @param notes cest un tableau des notes des etudiants
+ */
     // Affiche note et moyennes
     public void afficherNotesEtMoyenne(String nomEtudiant, int[] notes) {
-        // Affichage des notes
-        System.out.println("Notes de " + nomEtudiant + ":");
-        for (int note : notes) {
-            System.out.println("- " + note);
-        }
+       
+       //Affichage de la note
+       afficherNotes(nomEtudiant,notes);
 
-        // Calcul de la somme des notes
+       //Affichage de la moyenne
+       double Myn = calculMoyenne(notes);
+       System.out.printf("Moyenne de "+nomEtudiant+"est : "+notes);
+
+    }
+/**Affcihe les notes des etudiants
+ * 
+ * @param nomEtudiant qui est le nom de l'etudiant
+ * @param notes cest un tableau des notes des etudiants
+ */
+    //Affiche les notes
+    public void afficherNotes(String nomEtudiants , int[] notes){
+        System.out.println("Ntes de "+nomEtudiant+ ":");
+        for(int note : notes){
+            System.out.println("* "+note);
+        }
+    }
+
+
+/**Calcule la moyenne des etudiants
+ * 
+ * @param notes cest un tableau des notes des etudiants
+ * @return la moyenne des notes
+ */
+    //Calcule la moyenne mais retourne la moyenne aussi 
+    public void calculMoyenne(int[] notes){
         int somme = 0;
-        for (int note : notes) {
-            somme += note;
+        for(int note : notes){
+            somme += note ;
         }
-
-        // Affichage de la moyenne
-        double moyenne = (double) somme / notes.length;
-        System.out.println("Moyenne : " + moyenne);
+        return (double) somme /notes.length ;
     }
 }
